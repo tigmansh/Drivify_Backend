0f0388831518d70e6c1ffb6509882fabf886e10e
tigmansh <gargtigmanshu@gmail.com>
2023-02-22 16:44:43 +0530

    Testing Backend

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { userRouter } = require("./routes/user.routes");
+const { connection } = require("./configs/db");
+const { authentication } = require("./middleware/authenticate");
+const { bookingRouter } = require("./routes/booking.routes");
+const { adminRouter } = require("./routes/admin.routes");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+app.use("/admin", adminRouter);
+app.use("/users", userRouter);
+app.use(authentication);
+app.use("/bookings", bookingRouter);
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log(err.message);
+  }
+  console.log(`Server is running at port ${process.env.port}`);
+});
