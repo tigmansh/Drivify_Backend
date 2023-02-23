59e71a4f3fa1673925639d2ec024b6fc94589dbe
tigmansh <gargtigmanshu@gmail.com>
2023-02-23 14:05:12 +0530

    Some important changes in routes because of frontend !

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -8,31 +8,31 @@ const { bookingModel } = require("../models/booking.model");
 
 const adminRouter = express.Router();
 
-adminRouter.get("/users", async (req, res) => {
+adminRouter.post("/users", async (req, res) => {
   const { email, pass } = req.body;
   if (email === process.env.adminId && pass === process.env.adminPass) {
     try {
       const data = await userModel.find();
-      res.send(data);
+      res.send({ msg: data });
     } catch (err) {
-      res.send({ Error: err.message });
+      res.send({ err: err.message });
     }
   } else {
-    res.send({ Error: "You are not Authorized to go further." });
+    res.send({ err: "You are not Authorized to go further." });
   }
 });
 
-adminRouter.get("/bookings", async (req, res) => {
+adminRouter.post("/bookings", async (req, res) => {
   const { email, pass } = req.body;
   if (email === process.env.adminId && pass === process.env.adminPass) {
     try {
       const data = await bookingModel.find();
-      res.send(data);
+      res.send({ msg: data });
     } catch (err) {
-      res.send({ Error: err.message });
+      res.send({ err: err.message });
     }
   } else {
-    res.send({ Error: "You are not Authorized to go further." });
+    res.send({ err: "You are not Authorized to go further." });
   }
 });
 
